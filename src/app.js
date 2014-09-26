ef619d24e9ccd445ed8efbdeb6477ead2a443dfe
Anton Tumilovich <anton_tumilovich@nevicom.ru>
2014-09-26 15:56:27 +0400

    All Work

--- src/app.js
+++ src/app.js
@@ -1,122 +1,129 @@
 var UI = require('ui');
+//var sens_card = new UI.Card({title: 'Sensors :', icon: 'images/temp.png', body: ''});
 
-//var Vector2 = require('vector2');
-
-  var main = new UI.Menu({
-    sections: [{
-      items: [
-      {
-        title: 'Sensors',
-        icon: 'images/bulb.png'
-      },
-      {
-        title: 'Room light',
-        icon: 'images/bulb.png',
-//        subtitle: 'Light SW'
-      },
-      {
-        title: 'Hall light',
-        icon: 'images/bulb.png',
-      },
-      {
-        title: 'Toal light',
-        icon: 'images/bulb.png',
-      },
-      {
-        title: 'Bath light',
-        icon: 'images/bulb.png',
-      },
-      {
-        title: 'Kitch light',
-        icon: 'images/bulb.png',
-      },
-      {
-        title: 'Bath Fan',
-        icon: 'images/fan.png',
-      },
-      {
-        title: 'Kitch Fan',
-        icon: 'images/fan.png',
-      }]
+var main = new UI.Menu({
+  sections: [{
+    items:
+    [
+      {title: 'Sensors', icon: 'images/temp.png'},
+      {title: 'Room light', icon: 'images/bulb.png'},
+      {title: 'Hall light', icon: 'images/bulb.png'},
+      {title: 'Toal light', icon: 'images/bulb.png'},
+      {title: 'Bath light', icon: 'images/bulb.png'},
+      {title: 'Kitch light', icon: 'images/bulb.png'},
+      {title: 'Bath Fan', icon: 'images/fan.png'},
+      {title: 'Kitch Fan', icon: 'images/fan.png'},
+      {title: 'Refresh iCtrl', icon: 'images/home.png'}
+    ]
     }]
 });
 
-  var sens_card = new UI.Card({
-    title: 'Sensors :',
-    icon: 'images/bulb.png',
-    body: 'Room Temp : 20.6 C\nHall Temp : 21.1'
-  });
+var data_menu = new UI.Menu({
+  sections: [{
+    items:
+    [
+      {title: 'Room', icon: 'images/temp.png'},
+      {title: 'Out', icon: 'images/temp.png'},
+      {title: 'Bath', icon: 'images/temp.png'},
+      {title: 'Kitchen', icon: 'images/temp.png'},
+      {title: 'Door In', icon: 'images/fan.png'},
+      {title: 'Door Out', icon: 'images/fan.png'},
+    ]
+    }]
+});
 
+
+function refresh_switchs(e){
+  var ajax = require('ajax');
+      ajax({ url: 'http://ictrl.home:1110/jva6', type: 'json'},
+        function(data) {
+          main.item(0, 1, {subtitle: data.room_l });
+          main.item(0, 2, {subtitle: data.hall_l });
+          main.item(0, 3, {subtitle: data.toal_l });
+          main.item(0, 4, {subtitle: data.bath_l });
+          main.item(0, 5, {subtitle: data.kitch_l });
+          main.item(0, 6, {subtitle: data.bath_f });
+          main.item(0, 7, {subtitle: data.kitch_f });
+      }
+      );
+  console.log('Refresh switch : "');
+}
+
+function refresh_sensors(e){
+  var ajax = require('ajax');
+      ajax({ url: 'http://ictrl.home:1110/jva6', type: 'json'},
+        function(data) {
+          data_menu.item(0, 0, {subtitle: data.room_th });
+          data_menu.item(0, 1, {subtitle: data.out_th });
+          data_menu.item(0, 2, {subtitle: data.bath_th });
+          data_menu.item(0, 3, {subtitle: data.kitch_th });
+          data_menu.item(0, 4, {subtitle: data.door_in });
+          data_menu.item(0, 5, {subtitle: data.door_out });
+
+      }
+      );
+  console.log('Refresh sensors : "');
+}
+
+
+data_menu.on('select', function(e) {
+  refresh_sensors();
+});
 
 
 main.on('select', function(e) {
-//  var req = new XMLHttpRequest();
   var ajax = require('ajax');
   
-  
-  
-    if (e.itemIndex === 0 )
+      if (e.itemIndex === 0 )
     {
-//      ajax({ url: 'http://ictrl.home:1110/jva7', type: 'json' },
-
-      ajax({ url: 'http://ictrl.home:1110/jva7' },
-        function(data) {
-//            sens_data.title(data.contents.author);
-            sens_card.body(data);
-          console.log('FROM SERV ANSW : "' + data + '"');
-        }
-      );
- 
-        sens_card.show();
+      refresh_sensors();
+      data_menu.show();
     }
 
     if (e.itemIndex === 1 )
     {
-      ajax({ url: 'http://ictrl.home:1110/ts00' });
-
-//        function(data) {
-//            sens_data.title(data.contents.author);
- //           sens_card.body(data);
- //         console.log('FROM SERV ANSW : "' + data + '"');
- //       }
- //     );
+      ajax({ url: 'http://ictrl.home:1110/ts00', type: 'json' }, function(data) {main.item(0, 1, {subtitle: data.room_l });});
     }
 
     if (e.itemIndex === 2 )
     {
-      ajax({ url: 'http://ictrl.home:1110/ts01' });
+      ajax({ url: 'http://ictrl.home:1110/ts01', type: 'json' }, function(data) {main.item(0, 2, {subtitle: data.hall_l });});
     }
 
     if (e.itemIndex === 3 )
     {
-      ajax({ url: 'http://ictrl.home:1110/ts02' });
+      ajax({ url: 'http://ictrl.home:1110/ts02', type: 'json' }, function(data) {main.item(0, 3, {subtitle: data.toal_l });});
     }
 
     if (e.itemIndex === 4 )
     {
-      ajax({ url: 'http://ictrl.home:1110/ts03' });
+      ajax({ url: 'http://ictrl.home:1110/ts03', type: 'json' }, function(data) {main.item(0, 4, {subtitle: data.bath_l });});
     }
 
     if (e.itemIndex === 5 )
     {
-      ajax({ url: 'http://ictrl.home:1110/ts04' });
+      ajax({ url: 'http://ictrl.home:1110/ts04', type: 'json' }, function(data) {main.item(0, 5, {subtitle: data.kitch_l });});
     }
 
     if (e.itemIndex === 6 )
     {
-      ajax({ url: 'http://ictrl.home:1110/ts05' });
+      ajax({ url: 'http://ictrl.home:1110/ts05', type: 'json' }, function(data) {main.item(0, 6, {subtitle: data.bath_f });});
     }
 
     if (e.itemIndex === 7 )
     {
-      ajax({ url: 'http://ictrl.home:1110/ts06' });
+      ajax({ url: 'http://ictrl.home:1110/ts06', type: 'json' }, function(data) {main.item(0, 7, {subtitle: data.kitch_f });});
     }
 
-    
+    if (e.itemIndex === 8 )
+    {
+      refresh_switchs();
+    }
+
+
     console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
     console.log('The item is titled "' + e.item.title + '"');
   });
 
+refresh_switchs();
 main.show();
-
-
