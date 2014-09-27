49ae8fbbe1dc5b1276896bb824758c9c1b861af4
Anton Tumilovich <anton_tumilovich@nevicom.ru>
2014-09-27 22:14:43 +0400

    Request over local & internet

--- src/app.js
+++ src/app.js
@@ -1,15 +1,6 @@
 var UI = require('ui');
-var Accel = require('ui/accel');
 var ajax = require('ajax');
-Accel.init();
-
-
-Accel.on('tap', function(e) {
-  refresh_switchs();
-  refresh_sensors();
-  console.log('Tap event on axis: ' + e.axis + ' and direction: ' + e.direction);
-});
-
+var Accel = require('ui/accel'); Accel.init();
 
 var main = new UI.Menu({
   sections: [{
@@ -22,9 +13,7 @@ var main = new UI.Menu({
       {title: 'Bath light', icon: 'images/bulb.png'},
       {title: 'Kitch light', icon: 'images/bulb.png'},
       {title: 'Bath Fan', icon: 'images/fan.png'},
-      {title: 'Kitch Fan', icon: 'images/fan.png'},
-      {title: 'Refresh iCtrl', icon: 'images/home.png'}
-
+      {title: 'Kitch Fan', icon: 'images/fan.png'}
     ]
     }]
 });
@@ -44,9 +33,26 @@ var data_menu = new UI.Menu({
 });
 
 
-function refresh_switchs(e){
+
+
+function refresh_switchs(e)
+{
+  ajax({ url: 'http://ictrl.home:1110/jva6', type: 'json'},
+    function(data)
+    {
+      main.item(0, 1, {subtitle: data.room_l });
+      main.item(0, 2, {subtitle: data.hall_l });
+      main.item(0, 3, {subtitle: data.toal_l });
+      main.item(0, 4, {subtitle: data.bath_l });
+      main.item(0, 5, {subtitle: data.kitch_l });
+      main.item(0, 6, {subtitle: data.bath_f });
+      main.item(0, 7, {subtitle: data.kitch_f });
+    },
+    function()
+    {
       ajax({ url: 'http://www.nevicom.ru/cgi-bin/jva6', type: 'json'},
-        function(data) {
+        function(data)
+        {
           main.item(0, 1, {subtitle: data.room_l });
           main.item(0, 2, {subtitle: data.hall_l });
           main.item(0, 3, {subtitle: data.toal_l });
@@ -54,37 +60,47 @@ function refresh_switchs(e){
           main.item(0, 5, {subtitle: data.kitch_l });
           main.item(0, 6, {subtitle: data.bath_f });
           main.item(0, 7, {subtitle: data.kitch_f });
-      }
+        }           
       );
-  console.log('Refresh switch : "');
-}
-
-function refresh_sensors(e){
-  ajax({ url: 'http://www.nevicom.ru/cgi-bin/jva6', type: 'json'},
-      function(data) {
-        data_menu.item(0, 0, {subtitle: data.room_th });
-        data_menu.item(0, 1, {subtitle: data.out_th });
-        data_menu.item(0, 2, {subtitle: data.bath_th });
-        data_menu.item(0, 3, {subtitle: data.kitch_th });
-        data_menu.item(0, 4, {subtitle: data.door_in });
-        data_menu.item(0, 5, {subtitle: data.door_out });
-      }
+    }
   );
-  console.log('Refresh sensors : "');
 }
 
 
-data_menu.on('select', function(e) {
-  refresh_sensors();
-});
 
+
+function refresh_sensors(e)
+{
+  ajax({ url: 'http://ictrl.home:1110/jva6', type: 'json'}, 
+    function(data)
+    {
+      data_menu.item(0, 0, {subtitle: data.room_th });
+      data_menu.item(0, 1, {subtitle: data.out_th });
+      data_menu.item(0, 2, {subtitle: data.bath_th });
+      data_menu.item(0, 3, {subtitle: data.kitch_th });
+      data_menu.item(0, 4, {subtitle: data.door_in });
+      data_menu.item(0, 5, {subtitle: data.door_out });
+    },
+    function()
+    {         
+      ajax({ url: 'http://www.nevicom.ru/cgi-bin/jva6', type: 'json'}, 
+        function(data)
+        {
+          data_menu.item(0, 0, {subtitle: data.room_th });
+          data_menu.item(0, 1, {subtitle: data.out_th });
+          data_menu.item(0, 2, {subtitle: data.bath_th });
+          data_menu.item(0, 3, {subtitle: data.kitch_th });
+          data_menu.item(0, 4, {subtitle: data.door_in });
+          data_menu.item(0, 5, {subtitle: data.door_out });
+        }
+      );
+    }
+  );
+}
 
 main.on('select', function(e) {
   if (e.itemIndex === 0 )
-  {
-    refresh_sensors();
-    data_menu.show();
-  }
+  {data_menu.show(); refresh_sensors();}
 
   if (e.itemIndex === 1 ) {ajax({ url: 'http://ictrl.home:1110/ts00', type: 'json' }, function(data) {main.item(0, 1, {subtitle: data.room_l });});}
   if (e.itemIndex === 2 ) {ajax({ url: 'http://ictrl.home:1110/ts01', type: 'json' }, function(data) {main.item(0, 2, {subtitle: data.hall_l });});}
@@ -93,12 +109,12 @@ main.on('select', function(e) {
   if (e.itemIndex === 5 ) {ajax({ url: 'http://ictrl.home:1110/ts04', type: 'json' }, function(data) {main.item(0, 5, {subtitle: data.kitch_l });});}
   if (e.itemIndex === 6 ) {ajax({ url: 'http://ictrl.home:1110/ts05', type: 'json' }, function(data) {main.item(0, 6, {subtitle: data.bath_f });});}
   if (e.itemIndex === 7 ) {ajax({ url: 'http://ictrl.home:1110/ts06', type: 'json' }, function(data) {main.item(0, 7, {subtitle: data.kitch_f });});}
-  if (e.itemIndex === 8 ) {refresh_switchs();}
-
-
-  console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-  console.log('The item is titled "' + e.item.title + '"');
 });
 
-refresh_switchs();
+main.on('accelTap', function(e) {refresh_sensors();});
+data_menu.on('accelTap', function(e) {refresh_sensors();});
+data_menu.on('select', function(e) {refresh_sensors();});
+
+
 main.show();
+refresh_switchs();
